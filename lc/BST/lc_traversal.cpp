7d0947e89c45116e46f24185382fb34bf2cc35e3
幽幽的Yy <472860802@qq.com>
2022-11-27 14:23:06 +0800

    iasda

--- lc/BST/lc_traversal.cpp
+++ lc/BST/lc_traversal.cpp
@@ -23,21 +23,44 @@ public:
 
 };
 
-void pre_traversal(TreeNode *node, vector<int> &res) {
+void pre_traversal_recursive(TreeNode *node, vector<int> &res) {
     if (node == nullptr)
         return;
     res.push_back(node->val);
-    pre_traversal(node->left, res);
-    pre_traversal(node->right, res);
+    pre_traversal_recursive(node->left, res);
+    pre_traversal_recursive(node->right, res);
 
 }
 
 vector<int> preorderTraversal(TreeNode *root) {
     vector<int> res;
-    pre_traversal(root, res);
+    pre_traversal_recursive(root, res);
     return res;
 }
 
+vector<int> preorderTraversal_iteration(TreeNode *root) {
+    vector<int> res;
+    stack<TreeNode *> temp;
+    TreeNode *now;
+    if (root == nullptr)
+        return res;
+    else {
+        temp.push(root->right);
+        temp.push(root->left);
+        res.push_back(root->val);
+    }
+    while (!temp.empty()) {
+        now = temp.top();
+        temp.pop();
+        while (now != nullptr) {
+            res.push_back(now->val);
+            temp.push(now->right);
+            now = now->left;
+        }
+    }
+    return res;
+
+}
 
 int main() {
     TreeNode a = TreeNode(34);
