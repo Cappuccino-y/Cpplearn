8a0f85a279ce8f6d00ecf821621629eefce7ec60
yydyy77 <472860802>
2023-03-01 15:47:15 +0800

    some dp

---
+++ lc/DP/lc_change.cpp
@@ -0,0 +1,24 @@
+# include <set>
+# include <map>
+# include <vector>
+# include <iostream>
+# include <unordered_map>
+# include <unordered_set>
+# include <algorithm>
+# include <cmath>
+# include <stack>
+# include <queue>
+
+using namespace std;
+
+int change(int amount, vector<int> &coins) {
+    vector<int> dp(amount + 1);
+    dp[0] = 1;
+    for (int i = 0; i < coins.size(); ++i) {
+        for (int j = coins[i]; j <= amount; ++j) {
+            dp[j] += dp[j - coins[i]];
+        }
+    }
+    return dp[amount];
+}
+
---
+++ lc/DP/lc_combination_permute.cpp
@@ -0,0 +1,29 @@
+# include <set>
+# include <map>
+# include <vector>
+# include <iostream>
+# include <unordered_map>
+# include <unordered_set>
+# include <algorithm>
+# include <cmath>
+# include <stack>
+# include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int combinationSum4(vector<int> &nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) { // 遍历背包
+            for (int j = 0; j < nums.size(); j++) { // 遍历物品
+                if (i - nums[j] >= 0 && dp[i] < INT_MAX - dp[i - nums[j]]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+
